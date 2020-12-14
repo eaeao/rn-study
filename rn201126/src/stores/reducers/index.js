7ae2bd4cd7fef819752e8d6373c77e54164776d0
PLZHELPMETHX <33145510+PLZHELPMETHX@users.noreply.github.com>
2020-12-14 13:34:15 +0900

    Update index.js

--- rn201126/src/stores/reducers/index.js
+++ rn201126/src/stores/reducers/index.js
@@ -1,6 +1,6 @@
 import {combineReducers} from 'redux';
-import ColorReducers from './ColorReducers';
+import saturationReducers from './ColorReducers';
 
 export default combineReducers({
-  colorcount: ColorReducers,
+  colorcount: saturationReducers,
 });
