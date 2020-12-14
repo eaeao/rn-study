8067f408bca4fd5e7f0f637c1e258a2d96939163
PLZHELPMETHX <33145510+PLZHELPMETHX@users.noreply.github.com>
2020-12-14 14:54:44 +0900

    Update CountReducers.js

---
+++ rn201126/src/stores/reducers/CountReducers.js
@@ -0,0 +1,12 @@
+import {types} from '../actions/CountAction';
+
+export default (state = 0, action) => {
+  switch (action.type) {
+    case types.COUNT_UP:
+      return state + action.payload;
+    case types.COUNT_DOWN:
+      return state - action.payload;
+    default:
+      return state;
+  }
+};
